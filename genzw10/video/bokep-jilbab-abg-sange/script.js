1eeeb0a71fb8b5569eea08381fdda55f5e866dcb
genzw10 <168239653+genzw10@users.noreply.github.com>
2024-04-27 06:52:47 +0700

    Add files via upload

---
+++ genzw10/video/bokep-jilbab-abg-sange/script.js
@@ -0,0 +1,39 @@
+import videoList from '../../dqtq.js';
+
+function addVideo(video) {
+    const { id, vidSrc, judul, views, downloadUrl } = video;
+    const contentDiv = document.getElementById("content");
+    const down = document.getElementById("myButton");
+    
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("ca1", "ca2");
+    newDiv.id = id;
+    down.setAttribute("onclick", `window.location.href = '${downloadUrl}'`);
+
+    newDiv.innerHTML = `
+        <span class="thumb"><span class="center-poster"><iframe class="thumb" width="600" height="480" src="${vidSrc}" scrolling="no" frameborder="0" allowfullscreen="true"></iframe></span>
+        <div class="tittle-video">
+            <span class="bungkus-judul">
+                <span class="nama-video"><h3 id="judul_video_${id}" style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; cursor: pointer;">${judul}</h3>
+                <span class="palg"><p style="cursor: pointer;">GenZW • <span id="view_video_${id}">${views}</span>K Ditonton</p></span>
+            </span>
+            <span class="kanan"></span>
+        </div>
+        </span>
+    `;
+    
+    contentDiv.appendChild(newDiv);
+}
+
+// Tersisa kode untuk fungsi searchVideos dan event listener
+
+// Kode untuk menambahkan video
+
+// Mendapatkan data hanya untuk video ke-14
+// Buat salinan independen dari videoList menggunakan spread operator
+const videoToDisplay = [videoList[19]];
+
+// Menambahkan hanya video ke-14
+videoToDisplay.forEach(video => {
+    addVideo(video);
+});
---
+++ genzw10/video/v_17/script.js
@@ -0,0 +1,44 @@
+
+function addVideo(id, thumbSrc, judul, views, videoUrl) {
+    const contentDiv = document.getElementById("content");
+document.getElementById("myButton");
+    
+    const newDiv = document.createElement("div");
+    const down = document.getElementById("myButton");
+    newDiv.classList.add("ca1");
+    newDiv.classList.add("ca2")
+    newDiv.id = id;
+    down.setAttribute("onclick", `window.location.href = '${videoUrl}'`);
+    
+    newDiv.innerHTML = `
+        <span class="thumb"><span class="center-poster"><iframe class="thumb" width="600" height="480" src="${thumbSrc}" scrolling="no" frameborder="0" allowfullscreen="true"></iframe></span>
+        <div class="tittle-video">
+            <span class="bungkus-judul">
+                <span class="nama-video"><h3 id="judul_video_${id}" style="font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; cursor: pointer;">${judul}</h3>
+                <span class="palg"><p style="cursor: pointer;">GenZW • <span id="view_video_${id}">${views}</span>K Ditonton</p></span>
+            </span>
+        <span class="kanan"></span>
+        </div>
+        </span>
+    `;
+    
+    contentDiv.appendChild(newDiv);
+}
+
+function searchVideos() {
+    const input = document.getElementById("search-input").value.toLowerCase();
+    const videos = document.querySelectorAll(".ca1");
+    
+    videos.forEach(video => {
+        const judul = video.querySelector(".nama-video h3").innerText.toLowerCase();
+        if (judul.includes(input)) {
+            video.style.display = "block";
+        } else {
+            video.style.display = "none";
+        }
+    });
+}
+
+// Menambahkan video dengan id vdy-1
+addVideo("vdy-3", "https://d000d.com/e/8q8o5dv43n7x", "Bocil Bule Diewe Sama Ayah", "14.4", "https://d000d.com/d/8q8o5dv43n7x"
+    );
