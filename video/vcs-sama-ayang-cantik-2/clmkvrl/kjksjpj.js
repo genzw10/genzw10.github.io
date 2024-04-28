7f8f807041f16f18b831e861b744f37251ad2e39
genzw10 <168239653+genzw10@users.noreply.github.com>
2024-04-28 21:16:18 +0700

    Create kjksjpj.js

---
+++ video/vcs-sama-ayang-cantik-2/clmkvrl/kjksjpj.js
@@ -0,0 +1,98 @@
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ import videoList from '../../../dqtq.js';
+
+function addVideo(video) {
+    const { id, vidSrc, judul, views, downloadUrl, thumbSrc, size } = video;
+    const contentDiv = document.getElementById("content");
+    
+    
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("ca1", "ca2");
+    newDiv.id = id;
+
+    newDiv.innerHTML = `
+        <body><div class="downlod">
+                 <div class="info">
+                     <p>${judul}</p>
+        
+        <p>Size | <span style="background-color:#95e0ff;">${size} MB</span>></p>
+ <iframe class="responsive-iframe" src="${vidSrc}" frameborder="0" allowfullscreen></iframe>
+  
+        <h4>Untuk Mendownload video tahan Video Atau Klik titik 3 di pojok video dan download <h4>
+    <br><br>
+     
+        
+    </div>
+        </div>
+</body>
+<style>
+.download{
+    
+}
+    .info{
+        color:white;
+        
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    }
+    p,
+    h4{
+        text-align:center;
+    }
+</style>
+    `;
+    
+    contentDiv.appendChild(newDiv);
+}
+
+// Tersisa kode untuk fungsi searchVideos dan event listener
+
+// Kode untuk menambahkan video
+
+// Mendapatkan data hanya untuk video ke-14
+// Buat salinan independen dari videoList menggunakan spread operator
+const videoToDisplay = [videoList[26]];
+
+// Menambahkan hanya video ke-14
+videoToDisplay.forEach(video => {
+    addVideo(video);
+});
