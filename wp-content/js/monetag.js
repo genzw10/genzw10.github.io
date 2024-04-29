2ebed537e023ea60f65ef3a4919fc7944a98f3c6
genzw10 <168239653+genzw10@users.noreply.github.com>
2024-04-30 06:11:42 +0700

    Update monetag.js

--- wp-content/js/monetag.js
+++ wp-content/js/monetag.js
@@ -5,7 +5,7 @@ const directlinks = [
     "https://glaultoa.com/4/7299684",
     "https://debaucky.com/4/7299684",
     "https://keewoach.net/4/7413849",
-    "https://kaushooptawo.net/4/7299684",
+    "https://kaushooptawo.net/4/7299684"
 ];
 
 export { directlinks };
