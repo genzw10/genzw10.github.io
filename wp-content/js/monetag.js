dfac21e38246dd0d73161562b65adc5399486adc
genzw10 <168239653+genzw10@users.noreply.github.com>
2024-04-30 06:11:25 +0700

    Update monetag.js

--- wp-content/js/monetag.js
+++ wp-content/js/monetag.js
@@ -3,7 +3,9 @@ const directlinks = [
     "https://sheegiwo.com/4/7299684",
     "https://kuthoost.net/4/7299684",
     "https://glaultoa.com/4/7299684",
-    "https://debaucky.com/4/7299684"
+    "https://debaucky.com/4/7299684",
+    "https://keewoach.net/4/7413849",
+    "https://kaushooptawo.net/4/7299684",
 ];
 
 export { directlinks };
