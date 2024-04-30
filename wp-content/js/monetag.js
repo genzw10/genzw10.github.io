4660c80a7aa79cfc608ba18c58ab28f3b5adea1d
genzw10 <168239653+genzw10@users.noreply.github.com>
2024-05-01 05:41:17 +0700

    Update monetag.js

--- wp-content/js/monetag.js
+++ wp-content/js/monetag.js
@@ -1,9 +1,9 @@
 
 const directlinks = [
-    "https://sheegiwo.com/4/7299684",
-    "https://kuthoost.net/4/7299684",
-    "https://glaultoa.com/4/7299684",
-    "https://debaucky.com/4/7299684",
+    "https://meenetiy.com/4/7413849",
+    "https://intorterraon.com/4/7413849",
+    "https://bauptost.net/4/7413849",
+    "https://woapsogu.net/4/7413849",
     "https://keewoach.net/4/7413849",
     "https://kaushooptawo.net/4/7299684"
 ];
